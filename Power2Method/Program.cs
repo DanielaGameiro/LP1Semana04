13b6315dd4d0927f55dc4cb7285ceb8a2cab2078
Daniela Gameiro <danielagameiro@live.com.pt>
2023-03-22 13:18:57 +0000

    Add Power2Method project

---
+++ Power2Method/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Power2Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PowersOf2Until5();
+            PowersOf2Until5();
+        }
+
+        private static void PowersOf2Until5()
+        {
+            // precisa do 'i <<= 1' para evitar um infinite loop
+            for (int i = 1; i <= (1 << 5); i <<= 1)  
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
