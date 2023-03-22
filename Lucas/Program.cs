a617b97df5231b4abc361c92e902c807ebf09e93
Daniela Gameiro <danielagameiro@live.com.pt>
2023-03-22 15:49:00 +0000

    Add Lucas numbers exercise

---
+++ Lucas/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Lucas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            int n;
+            int l;
+
+            Console.Write("What Lucas number do you want? ");
+
+            s = Console.ReadLine();
+            n = int.Parse(s);
+            l = Lucas(n);
+
+            Console.WriteLine($"The {n}th Lucas number is {l}.");
+        }
+
+        private static int Lucas(int n)
+        {
+            int lucas;
+
+            if (n == 1)
+            {
+                lucas = 2;
+            }
+
+            else if (n == 2)
+            {
+                lucas = 1;
+            }
+            
+            else 
+            {
+                lucas = Lucas(n - 1) + Lucas(n - 2);
+            }
+
+            return lucas;
+        }
+    }
+}
