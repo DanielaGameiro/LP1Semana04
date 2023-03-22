730ec66c0d275b4714db6a6d46fa5a924c7c170a
Daniela Gameiro <danielagameiro@live.com.pt>
2023-03-22 14:22:17 +0000

    Add XML comments to the methods

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -4,6 +4,9 @@ namespace Power2Method
 {
     class Program
     {
+        /// <summary>
+        /// Program starts here. Calls the methods to print powers of 2.
+        /// </summary>
         static void Main(string[] args)
         {
             PowersOf2Until5();
@@ -15,11 +18,18 @@ namespace Power2Method
             PowersOf2UntilN(4);
         }
 
+        /// <summary>
+        /// Calls the method PowersOf2UntilN() to print powers of 2 until arg = 2.
+        /// </summary>
         private static void PowersOf2Until5()
         {
             PowersOf2UntilN(2);
         }
 
+        /// <summary>
+        /// Prints numbers from 1 to n.
+        /// </summary>
+        /// <param name="n">The maximum value to print to.</param>
         private static void PowersOf2UntilN(int n)
         {
             for (int i = 1; i <= (1 << n); i <<= 1)
