d77479c806c5d3f8cf511880a90175171ed7381f
Daniela Gameiro <danielagameiro@live.com.pt>
2023-03-22 13:51:37 +0000

    Add PowersOf2UntilN method to the program

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -7,13 +7,22 @@ namespace Power2Method
         static void Main(string[] args)
         {
             PowersOf2Until5();
+            Console.WriteLine();
             PowersOf2Until5();
+            Console.WriteLine();
+            PowersOf2UntilN(1);
+            Console.WriteLine();
+            PowersOf2UntilN(4);
         }
 
         private static void PowersOf2Until5()
         {
-            // precisa do 'i <<= 1' para evitar um infinite loop
-            for (int i = 1; i <= (1 << 5); i <<= 1)  
+            PowersOf2UntilN(2);
+        }
+
+        private static void PowersOf2UntilN(int n)
+        {
+            for (int i = 1; i <= (1 << n); i <<= 1)
             {
                 Console.WriteLine(i);
             }
